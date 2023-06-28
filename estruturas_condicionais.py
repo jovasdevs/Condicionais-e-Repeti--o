2c0727b74ed019f3b955b9f1b35a2ce2cc6654a0
jovasdevs <jovasdevs@gmail.com>
2023-06-28 17:00:12 -0300

    Adiconado 2 exercícios

--- estruturas_condicionais.py
+++ estruturas_condicionais.py
@@ -2,6 +2,7 @@ from ocrmypdf import InputFileError
 
 
 MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
 
 idade = int(input("Informe a sua idade: "))
 
@@ -16,3 +17,12 @@ if idade >= MAIOR_IDADE:
     print("Manior de idade, pode tirar a CNH")
 else:
     print("Ainda não pode tirar a CNH.")
+
+if idade >= MAIOR_IDADE:
+    print("Manior de idade, pode tirar a CNH")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer as aulas teóricas, mas não pode fazer as aulas práticas")
+else:
+    print("Ainda não pode tirar a CNH.")
+
+
